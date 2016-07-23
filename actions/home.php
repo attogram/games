b8e833d6406c4f8abb83256a00d3f9700eb94981
Attogram <barberi.office@gmail.com>
2016-07-23 17:42:06 +0200

    design updates

--- actions/home.php
+++ actions/home.php
@@ -1,5 +1,5 @@
 <?php
-// Attogram Framework - Games Module - Home v0.0.2
+// Attogram Framework - Games Module - Home v0.0.4
 
 namespace Attogram;
 
@@ -9,25 +9,24 @@ echo '<div class="container-fluid" style="background-color:#030303;">
 <div class="row hprow">
 
  <div class="col-sm-3 hpcol" style="background-color:black;">
-  <a href="pond/"><img src="web/pond/logo.png" width="178" height="100" alt="The Pond"></a>
-  <br /><br /><a href="pond/" class="hlite">The Pond &nbsp;
+  <a href="pond/"><img src="web/pond/logo.png" width="150" height="84" alt="The Pond"></a>
+  <br /><a href="pond/" class="hlite">The Pond &nbsp;
   <br /><small>A narrow fellow in the Pond</small></a>
  </div>
 
  <div class="col-sm-3 hpcol" style="background-color:black;">
   <a href="hextris/"><img src="web/hextris/logo.png" width="100" height="100" alt="Hextris"></a>
-  <br /><br /><a href="hextris/" class="hlite">Hextris</a>
-  <br />
+  <br /><a href="hextris/" class="hlite">Hextris</a>
  </div>
 
  <div class="col-sm-3 hpcol" style="background-color:black;">
   <a href="particle-clicker/"><img src="web/particle-clicker/logo.png" width="125" height="114" alt="Particle Clicker"></a>
-  <br /><br /><a href="particle-clicker/" class="hlite">Particle Clicker</a>
+  <br /><a href="particle-clicker/" class="hlite">Particle Clicker</a>
  </div>
 
  <div class="col-sm-3 hpcol" style="background-color:black;">
   <a href="candyjam/"><img src="web/candyjam/logo.png" width="150" width="119" alt="Candy Jam"></a>
-  <br /><br /><a href="candyjam/" class="hlite">Dental Defender &nbsp;
+  <br /><a href="candyjam/" class="hlite">Dental Defender &nbsp;
   <br /><small>Saga of the Candy Horde</small></a>
  </div>
 
@@ -36,7 +35,7 @@ echo '<div class="container-fluid" style="background-color:#030303;">
 
  <div class="col-sm-3 hpcol" style="background-color:black;">
   <a href="polybranch/"><img src="web/polybranch/logo.png" width="100" height="100" alt="PolyBranch"></a>
-  <br /><br /><a href="polybranch/" class="hlite">PolyBranch</a>
+  <br /><a href="polybranch/" class="hlite">PolyBranch</a>
  </div>
 
  <div class="col-sm-3 hpcol">coming soon...</div>
