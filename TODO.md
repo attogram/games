d2a03dd798ba2bf6518046d77d26cdc632794dbd
Attogram Project <attogram@users.noreply.github.com>
2019-04-21 00:40:39 +0200

    Attogram Games Website v1.0.1

---
+++ TODO.md
@@ -0,0 +1,28 @@
+# Future Developments
+
+* customizable index page title
+* customizable index page headline
+
+## Possible new games
+
+* https://github.com/alcalyn/openhex +npm
+* https://github.com/rx4hvn/snakes-and-ladders
+* https://bjulliana.github.io/StarshipTroopers-JS-Game/
+* https://github.com/PezCoder/Mr.Snake
+* https://github.com/dodosiz/html5-games
+* https://github.com/omnus/tiny-twitch
+* https://github.com/henshmi/Classic-Pool-Game
+* https://github.com/MahdiF/taptaptap
+* https://github.com/SimonHung/LodeRunner_TotalRecall
+* https://github.com/SimonHung/LodeRunner
+* https://github.com/aditya-r-m/twisty-polyhedra
+* https://github.com/gd4Ark/star-battle
+* 
+* https://github.com/erhankilic/snake-game
+* https://github.com/chrisheninger/matchimals.fun
+* https://github.com/luxedo/pong-almost-from-scratch
+* https://github.com/SimonHung/Klotski
+* https://github.com/jalbam/yasminoku
+* https://github.com/TiagoSilvaPereira/endless-runner-3d
+
+
--- build/games.php
+++ build/games.php
@@ -1,61 +1,100 @@
 <?php
+// Attogram Games Website Configuration
 
+// HTML <title>
+$title = 'Attogram Games Website';
+
+// HTML <h1>
+$headline = 'Attogram Games Website';
+
+// The Ordered List of Games
 $games = [
     'hextris' => [
-        'name' => 'Hextris',
-        'tag'  => 'hexagonal tetris',
-        'git'  => 'https://github.com/Hextris/hextris.git',
-    ],
-    'particle-clicker' => [
-        'name' => 'Particle Clicker',
-        'tag'  => 'be like cern',
-        'git'  => 'https://github.com/particle-clicker/particle-clicker.git',
+        'name'    => 'Hextris',
+        'tag'     => 'hexagonal tetris',
+        'git'     => 'https://github.com/Hextris/hextris.git',
+        'mobile'  => true,
+        'desktop' => true,
+
     ],
     'pond' => [
-        'name' => 'The Pond',
-        'tag'  => 'eat, swim, love',
-        'git'  => 'https://github.com/Zolmeister/pond.git',
-    ],
-    'html5-asteroids' => [
-        'name' => 'Asteroids',
-        'tag'  => 'retro asteroids',
-        'git'  => 'https://github.com/dmcinnes/HTML5-Asteroids.git',
-    ],
-    'raging-gardens' => [
-        'name' => 'Raging Gardens',
-        'tag'  => 'farting ninja rabbits',
-        'git'  => 'https://github.com/alunix/RagingGardensFB.git',
-    ],
-    'polybranch' => [
-        'name' => 'PolyBranch',
-        'tag'  => 'fly thru trees',
-        'git'  => 'https://github.com/gbatha/PolyBranch',
-        'index' => 'polybranchweb/index.html',
-    ],
-    'pacman' => [
-        'name' => 'pacman',
-        'tag'  => 'eat the dots',
-        'git'  => 'https://github.com/mumuy/pacman.git',
-    ],
-    'chess' => [
-        'name' => 'Chess',
-        'tag'  => 'e2 to e4',
-        'git'  => 'https://github.com/kbjorklu/chess.git',
-        'index' => 'chess.html',
-    ],
-    'dead-valley' => [
-        'name' => 'Dead Valley',
-        'tag'  => 'survival',
-        'git'  => 'https://github.com/dmcinnes/dead-valley.git',
+        'name'    => 'The Pond',
+        'tag'     => 'eat, swim, love',
+        'git'     => 'https://github.com/Zolmeister/pond.git',
+        'mobile'  => true,
+        'desktop' => true,
     ],
     '2048' => [
-        'name' => '2048',
-        'tag'  => '2, 4, 6, 8, swipe',
-        'git'  => 'https://github.com/gabrielecirulli/2048',
+        'name'    => '2048',
+        'tag'     => '2, 4, 6, 8, swipe',
+        'git'     => 'https://github.com/gabrielecirulli/2048',
+        'mobile'  => true,
+        'desktop' => true,
+    ],
+    'particle-clicker' => [
+        'name'    => 'Particle Clicker',
+        'tag'     => 'be like cern',
+        'git'     => 'https://github.com/particle-clicker/particle-clicker.git',
+        'mobile'  => true,
+        'desktop' => true,
+    ],
+    'html5-asteroids' => [
+        'name'    => 'Asteroids',
+        'tag'     => 'retro asteroids',
+        'git'     => 'https://github.com/dmcinnes/HTML5-Asteroids.git',
+        'mobile'  => false,
+        'desktop' => true,
+    ],
+    'raging-gardens' => [
+        'name'    => 'Raging Gardens',
+        'tag'     => 'farting ninja rabbits',
+        'git'     => 'https://github.com/alunix/RagingGardensFB.git',
+        'mobile'  => false,
+        'desktop' => true,
+    ],
+    'polybranch' => [
+        'name'    => 'PolyBranch',
+        'tag'     => 'fly thru trees',
+        'git'     => 'https://github.com/gbatha/PolyBranch',
+        'index'   => 'polybranchweb/index.html',
+        'mobile'  => false,
+        'desktop' => true,
+    ],
+    'pacman' => [
+        'name'    => 'pacman',
+        'tag'     => 'eat the dots',
+        'git'     => 'https://github.com/mumuy/pacman.git',
+        'mobile'  => false,
+        'desktop' => true,
+    ],
+    'chess' => [
+        'name'    => 'Chess',
+        'tag'     => 'e2 to e4',
+        'git'     => 'https://github.com/kbjorklu/chess.git',
+        'index'   => 'chess.html',
+        'mobile'  => true,
+        'desktop' => true,
+    ],
+    'dead-valley' => [
+        'name'    => 'Dead Valley',
+        'tag'     => 'survival',
+        'git'     => 'https://github.com/dmcinnes/dead-valley.git',
+        'mobile'  => false,
+        'desktop' => true,
     ],
     'hexgl' => [
-        'name' => 'HexGL',
-        'tag'  => 'racing pod',
-        'git'  => 'https://github.com/BKcore/HexGL.git',
+        'name'    => 'HexGL',
+        'tag'     => 'racing pod',
+        'git'     => 'https://github.com/BKcore/HexGL.git',
+        'mobile'  => false,
+        'desktop' => false,
+    ],
+    'phaser-piano' => [
+        'name'    => 'Piano',
+        'tag'     => 'ting ting',
+        'git'     => 'https://github.com/wasi0013/Phaser-Piano',
+        'index'   => 'Ting%20Ting/index.html',
+        'mobile'  => false,
+        'desktop' => true,
     ],
 ];
