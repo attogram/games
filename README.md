11b6c3caeccc5e93530c0fbf3c2fa032df27e277
Attogram Project <attogram@users.noreply.github.com>
2019-04-28 00:25:53 +0200

    Attogram Games Website v2.0.0 - new build support

--- README.md
+++ README.md
@@ -11,10 +11,45 @@ Every game included is open source and free to play!
 
 Live Demo: **<https://fosiper.com/games/>**
 
+## The Games
+
+The Attogram Games Website builder automates the installation 
+of the following games:
+
+* 2048 - <https://github.com/gabrielecirulli/2048>
+* 3d.city - <https://github.com/lo-th/3d.city>
+* 8 Queens - <https://github.com/attogram/8queens>
+* Chess - <https://github.com/attogram/chess>
+* Classic Pool Game - <https://github.com/henshmi/Classic-Pool-Game>
+* Dead Valley - <https://github.com/dmcinnes/dead-valley>
+* HexGL Lite - <https://github.com/attogram/HexGL-lite>
+* Hextris Lite - <https://github.com/attogram/hextris-lite>
+* HTML5 Asteroids - <https://github.com/dmcinnes/HTML5-Asteroids>
+* Hyperspace Garbage Collection - <https://github.com/razh/game-off-2013>
+* Klotski - <https://github.com/SimonHung/Klotski>
+* Lode Runner Total Recall - <https://github.com/SimonHung/LodeRunner_TotalRecall>
+* Missile Game - <https://github.com/bwhmather/missile-game>
+* pacman - <https://github.com/mumuy/pacman>
+* Particle Clicker - <https://github.com/particle-clicker/particle-clicker>
+* Phaser Cat - <https://github.com/DaxChen/phaser-cat>
+* PolyBranch - <https://github.com/gbatha/PolyBranch>
+* The Pond - <https://github.com/Zolmeister/pond>
+* Raging Gardens - <https://github.com/alunix/RagingGardensFB>
+* Tap Tap Tap - <https://github.com/MahdiF/taptaptap>
+* Twisty Polyhedra - <https://github.com/aditya-r-m/twisty-polyhedra>
+* Underrun - <https://github.com/phoboslab/underrun>
+* Wolf3d - <https://github.com/jseidelin/wolf3d>
+
+and some web musical instruments:
+
+* Javascript Piano - <https://github.com/mrcoles/javascript-piano>
+* Phaser Piano - <https://github.com/wasi0013/Phaser-Piano>
+* Virtual Piano - <https://github.com/otanim/virtual-piano>
+
 ## Installation
 
 * You'll need `git` and `php` to install the Games Website
-* You'll optionally need `composer`, `npm` and/or `yarn` to install some of the games
+* You'll optionally need `composer`, `npm` and/or `yarn`
 * Install the Games Website into a web accessible directory:
   * `git clone https://github.com/attogram/games.git games`
 * Run the build script:
--- _build/build.php
+++ _build/build.php
@@ -1,94 +1,112 @@
 <?php
-// Attogram Games Website
-// Build Script
+// Attogram Games Website Builder - https://github.com/attogram/games
 
-const VERSION = '1.2.7';
+const VERSION = '2.0.0';
 
 $title = 'Attogram Games Website';
-print  "$title " . VERSION . " - Build Script\n";
+print  "$title Builder v" . VERSION . "\n";
 
-$enableGit = true;
-$enableComposer = true;
+$writeEmbed = true; // also write an embeddable HTML menu of games
+$enableGit  = true;
 
-require_once 'games.php';
-print 'LOADING ' . count($games) . " GAMES\n";
+$enableGit ? syscall('git --version') : print "git Disabled\n";
 
 $buildDirectory = __DIR__ . DIRECTORY_SEPARATOR;
 print "BUILD DIRECTORY: $buildDirectory\n";
+if (!is_dir($buildDirectory)) {
+    exit("\nFATAL ERROR: Build Directory Not Found: $buildDirectory\n\n");
+}
+
+$gamesList = $buildDirectory . 'games.php';
+if (!is_readable($gamesList)) {
+    exit("\nFATAL ERROR: $gamesList NOT FOUND\n\n");
+}
+/** @noinspection PhpIncludeInspection */
+require_once $gamesList;
+print 'LOADING ' . count($games) . " GAMES\n";
 
 $homeDirectory = realpath($buildDirectory . '..') . DIRECTORY_SEPARATOR;
 print "HOME DIRECTORY: $homeDirectory\n";
-
-if (!@chdir($homeDirectory)) {
-    print "\nERROR: can not change directory to: $homeDirectory\n\n";
-
-    return;
+if (!is_dir($homeDirectory)) {
+    exit("\nFATAL ERROR: Home Directory Not Found: $homeDirectory\n\n");
 }
 
 $logoDirectory = $homeDirectory . '_logo' . DIRECTORY_SEPARATOR;
 print "LOGO DIRECTORY: $logoDirectory\n";
 
-$page = file_get_contents($buildDirectory . 'header.html');
+$header = file_get_contents($buildDirectory . 'header.html');
+
 $htmlTitle = $title ?? $title;
-$page = str_replace('{{TITLE}}', $htmlTitle, $page);
+$header = str_replace('{{TITLE}}', $htmlTitle, $header);
+
 $H1headline = $headline ?? $title;
-$page = str_replace('{{HEADLINE}}', $H1headline, $page);
+$header = str_replace('{{HEADLINE}}', $H1headline, $header);
+
+$css = file_get_contents($buildDirectory . 'css.css');
+$header = str_replace('{{CSS}}', "<style>$css</style>", $header);
+
+$menu = '<div class="list">';
 
 clearstatcache();
 
 foreach ($games as $index => $game) {
     $gameDirectory = $homeDirectory . $index;
 
-    print 'GAME: ' . $game['name'] . ": $gameDirectory\n";
+    print "GAME: {$game['name']} $gameDirectory {$game['git']}\n";
 
-    if (!is_dir($gameDirectory)) {
+    if ($enableGit && !is_dir($gameDirectory)) {
         chdir($homeDirectory);
         syscall('git clone ' . $game['git'] . ' ' . $index);
+
+        chdir($gameDirectory);
+        if (!empty($game['build'])) {
+            foreach ($game['build'] as $build) {
+                syscall($build);
+            }
+        }
     }
 
-    if (!@chdir($gameDirectory)) {
-        print "\nERROR: can not change directory to: $gameDirectory\n\n";
+
+    if (!chdir($gameDirectory)) {
+        print "\nERROR: GAME DIRECTORY NOT FOUND: $gameDirectory\n\n";
         continue;
     }
 
-    syscall('git pull');
-
-    if (!empty($game['composer']) && $game['composer'] && $enableComposer) {
-        syscall('composer install');
+    if ($enableGit) {
+        syscall('git pull');
     }
 
     $link = $index . '/';
     if (!empty($game['index'])) {
         $link .= $game['index'];
     }
-    $mobile = $desktop = '';
+    $mobile = '';
+    $desktop = '';
     if (!empty($game['mobile'])) {
-        $mobile = '📱';
+        $mobile = '&#128241;'; // 📱
     }
     if (!empty($game['desktop'])) {
-        $desktop = '⌨';
+        $desktop = '&#9000;'; // ⌨
     }
     $logo = is_readable($logoDirectory . $index . '.png')
         ? $index . '.png'
         : 'game.png';
-    $page .= '<a href="' . $link . '"><div class="game"><img src="_logo/' . $logo
+    $menu .= '<a href="' . $link . '"><div class="game"><img src="_logo/' . $logo
         . '" width="100" height="100" alt="' . $game['name'] . '"><br />' . $game['name']
         . '<br /><small>' . $game['tag'] . '</small>'
         . '<br /><div class="platform">' . $desktop . ' ' . $mobile . '</div>'
         . '</div></a>';
 }
 
-$page .= file_get_contents($buildDirectory . 'footer.html');
-$page = str_replace('{{VERSION}}', 'v' . VERSION, $page);
+$menu .= '</div>';
 
-print "WRITING {$homeDirectory}index.html\n";
-$indexWrote = file_put_contents($homeDirectory . 'index.html', $page);
-print "WROTE $indexWrote CHARACTERS\n\n";
+$footer = file_get_contents($buildDirectory . 'footer.html');
+$footer = str_replace('{{VERSION}}', 'v' . VERSION, $footer);
 
-if (!$indexWrote) {
-    print "ERROR WRITING TO {$homeDirectory}index.html\n";
-    print "DUMPING index.html\n\n\n";
-    print $page . "\n\n\n";
+write($homeDirectory . 'index.html', $header . $menu . $footer);
+
+if ($writeEmbed) {
+    write($homeDirectory . 'games.html', "<style>$css</style>$menu\n");
 }
 
 /**
@@ -97,4 +115,19 @@ if (!$indexWrote) {
 function syscall(string $command) {
     print "SYSTEM: $command\n";
     system($command);
+}
+
+/**
+ * @param string $filename
+ * @param string $contents
+ */
+function write(string $filename, string $contents)
+{
+    print "WRITING $filename\n";
+    $wrote = file_put_contents($filename, $contents);
+    print "WROTE $wrote CHARACTERS\n";
+    if (!$wrote) {
+        print "ERROR WRITING TO $filename\n";
+        print "DUMPING:\n\n\n$contents\n\n\n";
+    }
 }
